b09ab019a84b115b807e3094167aec4f679f0b3f
wp <wupan@analysys.com.cn>
2019-12-13 22:31:36 +0800

    aar单独混淆配置

--- make.sh
+++ make.sh
@@ -1,13 +1,14 @@
 #aar的maven包
 ./gradlew clean
 ./gradlew delRelease
-#./gradlew ans-sdk:analysys_core:uploadArchives
-#./gradlew ans-sdk:analysys_encryption:uploadArchives
-#./gradlew ans-sdk:analysys_push:uploadArchives
-#./gradlew ans-sdk:analysys_visual:uploadArchives
-#./gradlew ans-sdk:analysys_allgro:uploadArchives
-#/gradlew ans-sdk:analysys_arkanalysys:uploadArchives
-./gradlew uploadArchives
+./gradlew ans-sdk:analysys_core:uploadArchives
+./gradlew ans-sdk:analysys_encryption:uploadArchives
+./gradlew ans-sdk:analysys_push:uploadArchives
+./gradlew ans-sdk:analysys_visual:uploadArchives
+./gradlew ans-sdk:analysys_allgro:uploadArchives
+./gradlew ans-sdk:analysys_arkanalysys:uploadArchives
+
+#./gradlew uploadArchives
 
 #aar和jar包
 ./gradlew releaseMapping
