555c6dbcbaedd0a1f0c90a009ddc797adeaf98ab
wp <wupan@analysys.com.cn>
2019-12-30 13:21:59 +0800

    针对SharedPreference不兼容老版本修改

--- ans-sdk/analysys_core/src/main/java/com/analysys/utils/SharedUtil.java
+++ ans-sdk/analysys_core/src/main/java/com/analysys/utils/SharedUtil.java
@@ -18,7 +18,7 @@ import com.analysys.database.EventTableMetaData;
 public class SharedUtil {
 
     public static boolean getBoolean(Context context, String key, boolean defValue) {
-        String strValues = getString(context,key,"");
+        String strValues = getString(context,key,"","boolean");
         boolean values = defValue;
         if(!TextUtils.isEmpty(strValues)){
             values =  Boolean.parseBoolean(strValues);
@@ -28,7 +28,7 @@ public class SharedUtil {
 
     public static float getFloat(Context context, String key, float defValue) {
         float values = defValue;
-        String strValues = getString(context, key, "");
+        String strValues = getString(context, key, "","float");
         if (!TextUtils.isEmpty(strValues)) {
             values = CommonUtils.parseFloat(strValues, defValue);
         }
@@ -37,7 +37,7 @@ public class SharedUtil {
 
     public static int getInt(Context context, String key, int defValue) {
         int values = defValue;
-        String strValues = getString(context, key, "");
+        String strValues = getString(context, key, "","int");
         if (!TextUtils.isEmpty(strValues)) {
             values = CommonUtils.parseInt(strValues, defValue);
         }
@@ -46,7 +46,7 @@ public class SharedUtil {
 
     public static long getLong(Context context, String key, long defValue) {
         long values = defValue;
-        String strValues = getString(context, key, "");
+        String strValues = getString(context, key, "","long");
         if (!TextUtils.isEmpty(strValues)) {
             values = CommonUtils.parseLong(strValues, defValue);
         }
@@ -54,11 +54,15 @@ public class SharedUtil {
     }
 
     public static String getString(Context context, String key, String defValue) {
+        return getString(context, key, defValue, "string");
+    }
+
+    private static String getString(Context context, String key, String defValue,String type) {
         String values = null;
         Cursor cursor = null;
         try {
             Uri uri = EventTableMetaData.getTABLE_SP(context);
-            cursor = context.getContentResolver().query(uri, new String[]{key}, defValue, null, null);
+            cursor = context.getContentResolver().query(uri, new String[]{key,type}, defValue, null, null);
             if (cursor != null && cursor.getCount() >= 1) {
                 if (cursor.moveToPosition(0)) {
                     values = cursor.getString(0);
@@ -81,20 +85,60 @@ public class SharedUtil {
 
     public static void setBoolean(Context context, String key, boolean value) {
 
-        setString(context,key,String.valueOf(value));
+        try {
+            Uri uri = EventTableMetaData.getTABLE_SP(context);
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("key", key);
+            contentValues.put("values", value);
+            contentValues.put("type","boolean");
+
+            context.getContentResolver().insert(uri, contentValues);
+        } catch (Throwable ignore) {
+            ExceptionUtil.exceptionThrow(ignore);
+        }
     }
 
     public static void setFloat(Context context, String key, float value) {
-        setString(context,key,String.valueOf(value));
+        try {
+            Uri uri = EventTableMetaData.getTABLE_SP(context);
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("key", key);
+            contentValues.put("values", value);
+            contentValues.put("type","float");
+
+            context.getContentResolver().insert(uri, contentValues);
+        } catch (Throwable ignore) {
+            ExceptionUtil.exceptionThrow(ignore);
+        }
     }
 
     public static void setInt(Context context, String key, int value) {
-        setString(context,key,String.valueOf(value));
+        try {
+            Uri uri = EventTableMetaData.getTABLE_SP(context);
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("key", key);
+            contentValues.put("values", value);
+            contentValues.put("type","int");
+
+            context.getContentResolver().insert(uri, contentValues);
+        } catch (Throwable ignore) {
+            ExceptionUtil.exceptionThrow(ignore);
+        }
+
     }
 
     public static void setLong(Context context, String key, long value) {
+        try {
+            Uri uri = EventTableMetaData.getTABLE_SP(context);
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("key", key);
+            contentValues.put("values", value);
+            contentValues.put("type","long");
 
-        setString(context,key,String.valueOf(value));
+            context.getContentResolver().insert(uri, contentValues);
+        } catch (Throwable ignore) {
+            ExceptionUtil.exceptionThrow(ignore);
+        }
 
     }
 
@@ -105,6 +149,7 @@ public class SharedUtil {
             ContentValues contentValues = new ContentValues();
             contentValues.put("key", key);
             contentValues.put("values", value);
+            contentValues.put("type","string");
 
             context.getContentResolver().insert(uri, contentValues);
         } catch (Throwable ignore) {
